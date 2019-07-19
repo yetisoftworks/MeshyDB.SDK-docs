e574afb34067c2b24ff3860cfb2f219ca1251bba
Chris Pluta <chris.pluta@yetisoftworks.com>
2019-07-18 23:57:28 -0500

    Updating chat credentials

--- docs/scripts/chat.js
+++ docs/scripts/chat.js
@@ -2,7 +2,7 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/5ca4cabd6bba460528011170/default';
+s1.src='https://embed.tawk.to/5ca5797b1de11b6e3b06c9ea/1dg49dt2b';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);

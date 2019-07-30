6999658960bbbafc07307bb4b85c415a8f8d352f
Chris Pluta <chris.pluta@yetisoftworks.com>
2019-07-29 22:26:19 -0500

    Adding analytics through js file

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'MeshyDB'
-copyright = '2019, Yeti Softworks'
+copyright = '2019, Yeti Softworks LLC'
 author = 'Yeti Softworks'
 
 # The short X.Y version
@@ -87,7 +87,7 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static', 'scripts/requirehttps.js', 'styles/parameters.css', 'scripts/insights.js', 'scripts/chat.js']
+html_static_path = ['_static', 'scripts/requirehttps.js', 'styles/parameters.css', 'scripts/insights.js', 'scripts/chat.js', 'scripts/googleanalytics.js']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -101,7 +101,7 @@ html_static_path = ['_static', 'scripts/requirehttps.js', 'styles/parameters.css
 
 html_css_files = ['parameters.css']
 
-html_js_files = ['requirehttps.js', 'insights.js', 'chat.js']
+html_js_files = ['requirehttps.js', 'insights.js', 'chat.js', 'googleanalytics.js']
 
 html_favicon = 'images/favicon-32.png'
 # -- Options for HTMLHelp output ---------------------------------------------

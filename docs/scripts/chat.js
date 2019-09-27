0689b4b5cda1bd61602d573dd1d026b4aa51708b
Chris Pluta <chris.pluta@yetisoftworks.com>
2019-09-27 14:16:47 -0500

    Changing chat agent

--- docs/scripts/chat.js
+++ docs/scripts/chat.js
@@ -1,9 +1,11 @@
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/5d314f5bbfcb827ab0cc973e/default';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
+s1.defer=true;
+s1.src='//js.hs-scripts.com/6392621.js';
 s0.parentNode.insertBefore(s1,s0);
 })();
+
+// <!-- Start of HubSpot Embed Code -->
+// <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/6392621.js"></script>
+// <!-- End of HubSpot Embed Code -->
